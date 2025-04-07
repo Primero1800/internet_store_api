28736b30fc2667a1b9f119cf36d10eaa73144d69
Primero1800 <Primero@inbox.ru>
2025-04-07 19:07:11 +0300

    feat(app) users router added

--- src/api/v1/users/views.py
+++ src/api/v1/users/views.py
@@ -1,3 +1,24 @@
 from fastapi import APIRouter
+from src.api.v1.auth.backend import fastapi_users
+from src.api.v1.users.schemas import (
+    UserRead,
+    UserUpdate,
+)
 
 router = APIRouter()
+
+
+# /api/v1/users/me GET
+# /api/v1/users/me PATCH
+# /api/v1/users/{id} GET
+# /api/v1/users/{id} PATCH
+# /api/v1/users/{id} DELETE
+router.include_router(
+    fastapi_users.get_users_router(
+        UserRead, UserUpdate
+    ),
+)
+
+
+
+
