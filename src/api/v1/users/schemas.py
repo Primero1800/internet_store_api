9085a9416577dbe0b83dc82b1d1066c98a2d130c
Primero1800 <Primero@inbox.ru>
2025-04-08 12:41:08 +0300

    feat(app) Users schemas modified

--- src/api/v1/users/schemas.py
+++ src/api/v1/users/schemas.py
@@ -1,28 +1,43 @@
-from typing import Annotated
+from datetime import datetime
+from typing import Annotated, Optional
 
 from fastapi_users import schemas
 from pydantic import Field
 
-
-class BaseUser():
-
-    firstname: Annotated[str | None, Field(
+base_firstname = Annotated[str | None, Field(
             max_length=50, min_length=2,
             default="John",
             description="User's firstname, used in application",
             title='User firstname'
     )]
 
-    lastname: Annotated[str | None, Field(
+base_lastname = Annotated[str | None, Field(
             max_length=50, min_length=2,
             default="Doe",
             description="User's lastname, used in application",
             title='User lastname'
     )]
 
+base_data_joined = Annotated[datetime, Field(
+        description="The time and date, when user joined service",
+        title='Data joined'
+    )]
+
+base_last_login = Annotated[datetime, Field(
+        description="The time and date, when user logged in service last time",
+        title='Last login'
+    )]
+
+
+class BaseUser():
+
+    firstname: base_firstname
+    lastname: base_lastname
+
 
 class UserRead(BaseUser, schemas.BaseUser[int]):
-    pass
+    data_joined: base_data_joined
+    last_login: base_last_login
 
 
 class UserCreate(BaseUser, schemas.BaseUserCreate):
@@ -30,4 +45,6 @@ class UserCreate(BaseUser, schemas.BaseUserCreate):
 
 
 class UserUpdate(BaseUser, schemas.BaseUserUpdate):
-    pass
+    last_login: Optional[base_last_login] = None
+    firstname: Optional[base_firstname] = None
+    lastname: Optional[base_lastname] = None
