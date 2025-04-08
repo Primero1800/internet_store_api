0751ae3e800b69552ff739eac1129200418a9b3f
Primero1800 <Primero@inbox.ru>
2025-04-08 12:05:44 +0300

    feat(app) verify endpoint added

--- src/api/v1/auth/views.py
+++ src/api/v1/auth/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import (
     APIRouter,
     status,
@@ -5,6 +7,8 @@ from fastapi import (
     Response, Depends, Body,
 )
 from fastapi_users import BaseUserManager, models, exceptions
+from fastapi_users.router import ErrorCode
+from fastapi_users.router.common import ErrorModel
 from pydantic import EmailStr
 
 from src.api.v1.auth.backend import (
@@ -12,6 +16,7 @@ from src.api.v1.auth.backend import (
     auth_backend,
 )
 from src.api.v1.auth.dependencies import get_user_manager
+from src.core.settings import settings
 from .service import AuthService
 from ..users.schemas import (
     UserRead,
@@ -19,7 +24,10 @@ from ..users.schemas import (
 )
 
 
+logger = logging.getLogger(__name__)
+
 router = APIRouter()
+
 
 # /login [POST]
 # /logout [POST]
@@ -55,9 +63,51 @@ async def request_verify_token(
     )
 
 
-router.include_router(
-    fastapi_users.get_verify_router(UserRead),
+@router.post(
+    "/verify",
+    response_model=UserRead,
+    name="verify:verify",
+    responses={
+        status.HTTP_400_BAD_REQUEST: {
+            "model": ErrorModel,
+            "content": {
+                "application/json": {
+                    "examples": {
+                        ErrorCode.VERIFY_USER_BAD_TOKEN: {
+                            "summary": "Bad token, not existing user or"
+                            "not the e-mail currently set for the user.",
+                            "value": {"detail": ErrorCode.VERIFY_USER_BAD_TOKEN},
+                        },
+                        ErrorCode.VERIFY_USER_ALREADY_VERIFIED: {
+                            "summary": "The user is already verified.",
+                            "value": {
+                                "detail": ErrorCode.VERIFY_USER_ALREADY_VERIFIED
+                            },
+                        },
+                    }
+                }
+            },
+        }
+    },
 )
+async def verify(
+    request: Request,
+    token: str = Body(..., embed=True),
+    user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+):
+    service = AuthService(
+        user_manager=user_manager
+    )
+    return await service.verify(
+        request=request,
+        token=token,
+        schema=UserRead,
+    )
+
+
+# router.include_router(
+#     fastapi_users.get_verify_router(UserRead),
+# )
 
 
 # /forgot-password
@@ -65,3 +115,26 @@ router.include_router(
 router.include_router(
     fastapi_users.get_reset_password_router(),
 )
+
+
+@router.get(
+    "/verify-hook",
+    name="verify:verify-token-hook",
+    response_model=UserRead
+)
+async def hook_verify(
+    request: Request,
+    path: str,
+    user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+) -> UserRead:
+
+    logger.warning("In verify-hook: got token from outer link")
+
+    service = AuthService(
+        user_manager=user_manager
+    )
+    return await service.verify(
+        request=request,
+        token=path,
+        schema=UserRead,
+    )
