546c213ed28b930ad199b124b0b013cefbf005db
Primero1800 <Primero@inbox.ru>
2025-04-09 11:04:54 +0300

    feat(app) pagination scrypt added

---
+++ src/scrypts/pagination.py
@@ -0,0 +1,22 @@
+from typing import Optional, Any, Sequence
+
+
+def sort_key(x: dict, sort_by: str):
+    x = str(x.get(sort_by, 0))
+    return x
+
+
+async def paginate_result(
+    query_list: Sequence[Any] | list[Any],
+    page: Optional[int] = 1,
+    size: Optional[int] = 10,
+    sort_by: Optional[str] = None,
+) -> Sequence | list[Any]:
+
+    if sort_by:
+        query_list = sorted(query_list, key=lambda x: sort_key(x, sort_by))
+
+    start_index = (page - 1) * size
+    end_index = start_index + size
+
+    return query_list[start_index:end_index]
