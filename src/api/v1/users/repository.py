362fc64407e098021d9c90e04e7b0e9461991e90
Primero1800 <Primero@inbox.ru>
2025-04-09 12:13:36 +0300

    feat(app) Users service and repository added

---
+++ src/api/v1/users/repository.py
@@ -0,0 +1,35 @@
+import logging
+
+from fastapi import status
+from fastapi_users import BaseUserManager, models
+from sqlalchemy import select, Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.api.v1.users.exceptions import NoSessionException
+from src.core.models import User
+
+logger = logging.getLogger(__name__)
+
+class UsersRepository:
+    def __init__(
+        self,
+        session: AsyncSession | None = None,
+        user_manager: BaseUserManager[models.UP, models.ID] | None = None,
+    ):
+        self.user_manager = user_manager
+        self.session = session
+
+    async def get_all_users(self):
+        if not self.session:
+            logger.error(
+                "Impossible to get all users from database, because no active session. "
+                "Raised NoSessionException"
+            )
+            raise NoSessionException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                msg="No active sessions found"
+            )
+        stmt = select(User).order_by(User.id)
+        result: Result = await self.session.execute(stmt)
+        users = result.scalars().all()
+        return users
---
+++ src/api/v1/users/service.py
@@ -0,0 +1,47 @@
+import logging
+
+from fastapi import status
+from fastapi.responses import ORJSONResponse
+from fastapi_users import BaseUserManager, models
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .repository import UsersRepository
+from .exceptions import NoSessionException
+
+
+class UsersService:
+    def __init__(
+        self,
+        user_manager: BaseUserManager[models.UP, models.ID],
+        session: AsyncSession | None = None
+    ):
+        self.user_manager = user_manager
+        self.session = session
+        self.logger = logging.getLogger(__name__)
+
+    async def get_all_users(self):
+
+        repository: UsersRepository = UsersRepository(
+            session=self.session,
+        )
+        try:
+            result = await repository.get_all_users()
+            return result
+        except NoSessionException as exc:
+            return ORJSONResponse(
+                status_code=exc.status_code,
+                content={
+                    "message": "Handled by Service exception handler",
+                    "detail": exc.msg,
+                }
+            )
+        except IntegrityError as exc:
+            self.logger.error("Database error", exc_info=exc)
+            return ORJSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content={
+                    "message": "Handled by Service exception handler",
+                    "detail": "Internal server error while working with database",
+                }
+            )
