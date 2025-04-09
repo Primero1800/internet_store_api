46a18db7e62362f3f11b06c3b7785f85ee7ad3fa
Primero1800 <Primero@inbox.ru>
2025-04-09 16:27:33 +0300

    feat(app) rate_limit_config added

--- src/core/config/__init__.py
+++ src/core/config/__init__.py
@@ -1,3 +1,4 @@
 from .app_config import AppConfigurer
 from .swagger_config import SwaggerConfigurer
 from .database_config import DBConfigurer
+from .rate_limiter_config import RateLimiterConfigurer
--- src/core/config/rate_limiter_config.py
+++ src/core/config/rate_limiter_config.py
@@ -6,50 +6,53 @@ from fastapi import Request, status
 from fastapi.responses import ORJSONResponse
 
 
-def rate_limit(
-        max_calls: int,
-        period: int,
-):
-    def decorator(
-            func: Callable[[Request, Any], Any]
-    ) -> Callable[[Request, Any], Any]:
+class RateLimiterConfigurer:
 
-        usage: dict[str, list[float]] = {}
+    @staticmethod
+    def rate_limit(
+            max_calls: int,
+            period: int,
+    ):
+        def decorator(
+                func: Callable[[Request, Any], Any]
+        ) -> Callable[[Request, Any], Any]:
 
-        @wraps(func)
-        async def wrapper(request: Request, *args, **kwargs) -> Any:
-            if not request.client:
+            usage: dict[str, list[float]] = {}
+
+            @wraps(func)
+            async def wrapper(request: Request, *args, **kwargs) -> Any:
+                if not request.client:
+                    return ORJSONResponse(
+                        status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                        content={
+                            "message": "Handled by Rate Limiter exception handler",
+                            "detail": "Request has no client information",
+                        }
+                    )
+
+                ip_address: str = request.client.host
+                unique_id: str = hashlib.sha256(ip_address.encode()).hexdigest()
+                now = time.time()
+
+                if unique_id not in usage:
+                    usage[unique_id] = []
+
+                timestamps = usage[unique_id]
+                timestamps[:] = [t for t in timestamps if now - t < period]
+
+                if len(timestamps) < max_calls:
+                    timestamps.append(now)
+                    return await func(request, *args, **kwargs)
+
+                wait: float = period - (now - timestamps[0])
                 return ORJSONResponse(
-                    status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                    status_code=status.HTTP_429_TOO_MANY_REQUESTS,
                     content={
                         "message": "Handled by Rate Limiter exception handler",
-                        "detail": "Request has no client information",
+                        "detail": f"Rate limit exceed. Retry after {wait:.2f} seconds",
                     }
                 )
 
-            ip_address: str = request.client.host
-            unique_id: str = hashlib.sha256(ip_address.encode()).hexdigest()
-            now = time.time()
+            return wrapper
 
-            if unique_id not in usage:
-                usage[unique_id] = []
-
-            timestamps = usage[unique_id]
-            timestamps[:] = [t for t in timestamps if now - t < period]
-
-            if len(timestamps) < max_calls:
-                timestamps.append(now)
-                return await func(request, *args, **kwargs)
-
-            wait: float = period - (now - timestamps[0])
-            return ORJSONResponse(
-                status_code=status.HTTP_429_TOO_MANY_REQUESTS,
-                content={
-                    "message": "Handled by Rate Limiter exception handler",
-                    "detail": f"Rate limit exceed. Retry after {wait:.2f} seconds",
-                }
-            )
-
-        return wrapper
-
-    return decorator
+        return decorator
