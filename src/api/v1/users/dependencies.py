924544a6db4fb867cc23d567650be9a5f1f3d9a1
Primero1800 <Primero@inbox.ru>
2025-04-10 10:24:03 +0300

    feat(app) dependency added

--- src/api/v1/users/dependencies.py
+++ src/api/v1/users/dependencies.py
@@ -1,5 +1,8 @@
-from src.api.v1.auth.backend import fastapi_users
+from fastapi import Depends, status, HTTPException
+from fastapi_users import models, BaseUserManager, exceptions
 
+from src.api.v1.auth.backend import fastapi_users
+from src.api.v1.auth.dependencies import get_user_manager
 
 current_user = fastapi_users.current_user(
     active=True,
@@ -17,3 +20,14 @@ current_user_or_none = fastapi_users.current_user(
     active=True,
     verified=True,
 )
+
+
+async def get_user_or_404(
+        id: str,
+        user_manager: BaseUserManager[models.UP, models.ID] = Depends(get_user_manager),
+) -> models.UP:
+    try:
+        parsed_id = user_manager.parse_id(id)
+        return await user_manager.get(parsed_id)
+    except (exceptions.UserNotExists, exceptions.InvalidID) as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND) from e
