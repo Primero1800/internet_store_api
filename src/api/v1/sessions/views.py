bba6aa74c1a159e19f73756e3f24f6cf9e9e9827
Primero1800 <Primero@inbox.ru>
2025-04-11 13:03:13 +0300

    feat(app) create_session endpoint added

--- src/api/v1/sessions/views.py
+++ src/api/v1/sessions/views.py
@@ -1,3 +1,26 @@
-from fastapi import APIRouter
+from typing import Any
+
+from fastapi import APIRouter, Request, Response, status, Depends
+
+from src.api.v1.users.dependencies import current_user_or_none
+from src.core.config import RateLimiter
+
+from .service import SessionsService
+
 
 router = APIRouter()
+
+
+@router.post(
+    "/create_session"
+)
+@RateLimiter.rate_limit()
+async def create_session(
+        request: Request,
+        response: Response,
+        user: Any = Depends(current_user_or_none),
+):
+    service: SessionsService = SessionsService()
+    return await service.create_session(
+
+    )
