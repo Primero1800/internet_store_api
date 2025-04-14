3a859f7f37cf76146218c9591d08bee17d270731
Primero1800 <Primero@inbox.ru>
2025-04-14 12:01:07 +0300

    app(app) alembic brand, brandimage added

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -2,7 +2,13 @@ __all__ = (
     "Base",
 
     "User",
+
+    "Brand",
+    "BrandImage",
+
 )
 
 from .base import Base
 from .users.models import User
+
+from .store.brand import Brand, BrandImage
