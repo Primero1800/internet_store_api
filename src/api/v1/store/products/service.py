9f8d94d88dfe305b35c6d4c93c89aa649db62d3f
Primero1800 <Primero@inbox.ru>
2025-04-15 17:54:07 +0300

    app(app) products utils and service added

---
+++ src/api/v1/store/products/service.py
@@ -0,0 +1,46 @@
+import logging
+from typing import TYPE_CHECKING
+
+from fastapi import UploadFile, status
+from fastapi.responses import ORJSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.tools.exceptions import CustomException
+from . import utils
+from .repository import ProductsRepository
+from .schemas import (
+    ProductCreate,
+    ProductUpdate,
+    ProductPartialUpdate,
+)
+from .exceptions import Errors
+from ..utils.image_utils import save_image
+
+if TYPE_CHECKING:
+    from src.core.models import Product
+    from .filters import ProductFilter
+
+CLASS = "Product"
+_CLASS = "product"
+
+
+class ProductsService:
+    def __init__(
+            self,
+            session: AsyncSession,
+    ):
+        self.session = session
+        self.logger = logging.getLogger(__name__)
+
+    async def get_all(
+            self,
+            filter_model: "ProductFilter",
+    ):
+        repository: ProductsRepository = ProductsRepository(
+            session=self.session
+        )
+        result = []
+        listed_orm_models = await repository.get_all(filter_model=filter_model)
+        for orm_model in listed_orm_models:
+            result.append(await utils.get_short_schema_from_orm(orm_model=orm_model))
+        return result
---
+++ src/api/v1/store/products/utils.py
@@ -0,0 +1,52 @@
+from typing import TYPE_CHECKING
+
+from .schemas import ProductRead, ProductShort
+from ..brands.schemas import BrandShort
+from ..rubrics.schemas import RubricShort
+
+if TYPE_CHECKING:
+    from src.core.models import Product
+
+
+async def get_schema_from_orm(
+    orm_model: "Product",
+    maximized: bool = False,
+) -> ProductRead:
+
+    # BRUTE FORCE VARIANT
+
+    short_schema: ProductShort = await get_short_schema_from_orm(orm_model=orm_model)
+
+    rubrics = []
+    for rubric in orm_model.rubrics:
+        image_file = rubric.image.file if rubric.image else ''
+        rubric: RubricShort = RubricShort(**rubric.to_dict(), image_file=image_file)
+        rubrics.append(rubric)
+
+    brand_image_file = orm_model.brand.image.file if orm_model.brand.image else ''
+    brand: BrandShort = BrandShort(
+        **orm_model.brand.to_dict(),
+        image_file=brand_image_file,
+    )
+
+    images = [image.file for image in orm_model.images]
+
+    return ProductRead(
+        **short_schema.model_dump(),
+        description=orm_model.description,
+        rubrics=rubrics,
+        brand=brand,
+        images=images,
+    )
+
+
+async def get_short_schema_from_orm(
+    orm_model: "Product"
+) -> ProductShort:
+    image_file = orm_model.images[0].file if orm_model.images and hasattr(orm_model.images[0], "file") else ''
+
+    # BRUTE FORCE VARIANT
+    return ProductShort(
+        **orm_model.to_dict(),
+        image_file=image_file,
+    )
