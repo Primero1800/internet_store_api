0af22ad76f5d04fca2ae61588b2dfd149d2d859e
Primero1800 <Primero@inbox.ru>
2025-04-16 13:53:48 +0300

    refactoring(app) product utils changed

--- src/api/v1/store/products/repository.py
+++ src/api/v1/store/products/repository.py
@@ -1,4 +1,5 @@
 import logging
+from fastapi import status
 from typing import Sequence, TYPE_CHECKING, Union
 
 from sqlalchemy import select, Result
@@ -55,6 +56,7 @@ class ProductsRepository:
             raise CustomException(
                 msg=f"{CLASS} with {text_error} not found"
             )
+        print("OOOOOOOOORRRRRM MODEL DICT", orm_model.to_dict())
         return orm_model
 
     async def get_one(
@@ -107,3 +109,90 @@ class ProductsRepository:
     ):
         orm_model: Product = Product(**instance.model_dump())
         return orm_model
+
+    async def create_one_empty(
+            self,
+            orm_model: Product
+    ):
+        try:
+            self.session.add(orm_model)
+            await self.session.commit()
+            await self.session.refresh(orm_model)
+            self.logger.info("%r %r was successfully created" % (CLASS, orm_model))
+        except IntegrityError as error:
+            self.logger.error(f"Error while orm_model creating", exc_info=error)
+            raise CustomException(
+                msg=Errors.already_exists_titled(orm_model.title)
+            )
+
+    async def create_one_with_relations(
+            self,
+            orm_model: Product,
+            rubric_orm_models: list[Rubric]
+    ):
+        try:
+            self.session.add(orm_model)
+            for rubric_orm_model in rubric_orm_models:
+                orm_model.rubrics.append(rubric_orm_model)
+            await self.session.commit()
+            await self.session.refresh(orm_model)
+            self.logger.info("%r %r was successfully created" % (CLASS, orm_model))
+            self.logger.info("Rubrics were successfully added to product")
+        except IntegrityError as error:
+            self.logger.error(f"Error while orm_model with relations creating", exc_info=error)
+            raise CustomException(
+                msg=Errors.already_exists_titled(orm_model.title)
+            )
+
+    async def create_product_image(
+            self,
+            file: str,
+            orm_model: Product
+    ):
+        try:
+            image: ProductImage | None = ProductImage(file=file, product_id=orm_model.id)
+            self.session.add(image)
+            await self.session.commit()
+            self.logger.info("%sImage %r was successfully created" % (CLASS, image))
+        except IntegrityError as error:
+            self.logger.error(
+                "Error occurred while saving data to database. Parent model will be deleted", exc_info=error
+            )
+            await self.delete_one(
+                orm_model=orm_model,
+            )
+            raise CustomException(
+                msg=f"Error while {image!r} creating."
+            )
+
+    async def add_rubrics_to_model(
+            self,
+            orm_model: Product,
+            rubric_orm_models: list[Rubric]
+    ):
+        try:
+            for rubric_orm_model in rubric_orm_models:
+                orm_model.rubrics.append(rubric_orm_model)
+            await self.session.commit()
+            await self.session.refresh(orm_model)
+            self.logger.info("Rubrics were successfully added to product")
+        except IntegrityError as exc:
+            self.logger.error(Errors.DATABASE_ERROR, exc_info=exc)
+            raise CustomException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                msg=Errors.DATABASE_ERROR
+            )
+
+    async def delete_one(
+            self,
+            orm_model: Product,
+    ) -> None:
+        try:
+            self.logger.info(f"Deleting %r from database" % orm_model)
+            await self.session.delete(orm_model)
+            await self.session.commit()
+        except IntegrityError as exc:
+            self.logger.error("Error while deleting data from database", exc_info=exc)
+            raise CustomException(
+                msg="Error while deleting %r from database" % orm_model
+            )
--- src/api/v1/store/products/utils.py
+++ src/api/v1/store/products/utils.py
@@ -19,25 +19,22 @@ async def get_schema_from_orm(
 
     short_schema: ProductShort = await get_short_schema_from_orm(orm_model=orm_model)
 
-    rubrics = []
+    rubrics_shorts = []
+    from ..rubrics.utils import get_short_schema_from_orm as get_short_rubric_schema_from_orm
     for rubric in orm_model.rubrics:
-        image_file = rubric.image.file if rubric.image else ''
-        rubric: RubricShort = RubricShort(**rubric.to_dict(), image_file=image_file)
-        rubrics.append(rubric)
+        rubrics_shorts.append(await get_short_rubric_schema_from_orm(rubric))
 
-    brand_image_file = orm_model.brand.image.file if orm_model.brand.image else ''
-    brand: BrandShort = BrandShort(
-        **orm_model.brand.to_dict(),
-        image_file=brand_image_file,
-    )
+    from ..brands.utils import get_short_schema_from_orm as get_short_brand_schema_from_orm
+    brand_short = await get_short_brand_schema_from_orm(orm_model.brand)
 
     images = [image.file for image in orm_model.images]
 
     return ProductRead(
-        **short_schema.model_dump(),
-        description=orm_model.description,
-        rubrics=rubrics,
-        brand=brand,
+        **orm_model.to_dict(),
+
+        rubrics=rubrics_shorts,
+        brand=brand_short,
+        image_file=await get_main_image_file(orm_model),
         images=images,
     )
 
@@ -45,13 +42,16 @@ async def get_schema_from_orm(
 async def get_short_schema_from_orm(
     orm_model: "Product"
 ) -> ProductShort:
-    image_file = orm_model.images[0].file if orm_model.images and hasattr(orm_model.images[0], "file") else ''
 
     # BRUTE FORCE VARIANT
     return ProductShort(
         **orm_model.to_dict(),
-        image_file=image_file,
+        image_file=await get_main_image_file(orm_model),
     )
+
+
+async def get_main_image_file(orm_model: "Product"):
+    return orm_model.images[0].file if orm_model.images and hasattr(orm_model.images[0], "file") else ''
 
 
 async def temporary_fragment(ids: str | list):
