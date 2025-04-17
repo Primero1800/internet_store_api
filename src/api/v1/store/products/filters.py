40cfa7a5c3d2b72838bbfecd1acca3a6c2cddfe5
Primero1800 <Primero@inbox.ru>
2025-04-17 12:59:13 +0300

    feat(app) product filters added

--- src/api/v1/store/products/filters.py
+++ src/api/v1/store/products/filters.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from typing import Optional
 
 from fastapi_filter.contrib.sqlalchemy import Filter
@@ -9,6 +10,20 @@ from src.core.models import Product
 class ProductFilter(Filter):
     order_by: Optional[list[str]] = ['id']
     title__like: Optional[str] = Field(default=None, description="Filter by title contains", )
+    discount__gte: Optional[int] = Field(default=None, description="Filter by discount contains", )
+    price__gte: Optional[Decimal] = Field(
+        default=None,
+        max_digits=8,
+        decimal_places=2,
+        description="Filter by price greater or equal",
+    )
+    price__lte: Optional[Decimal] = Field(
+        default=None,
+        max_digits=8,
+        decimal_places=2,
+        description="Filter by price less or equal",
+    )
+    available: Optional[bool] = Field(default=None, description="Filter if available")
     search_for: Optional[str] = None
 
     class Constants(Filter.Constants):
