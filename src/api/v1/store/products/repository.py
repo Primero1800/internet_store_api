6c98b427ada20b83ad79f22bc4dc9b094fcc8150
Primero1800 <Primero@inbox.ru>
2025-04-17 12:34:57 +0300

    feat(app) edit one with relations repository method added

--- src/api/v1/store/products/repository.py
+++ src/api/v1/store/products/repository.py
@@ -1,6 +1,6 @@
 import logging
 from fastapi import status
-from typing import Sequence, TYPE_CHECKING, Union
+from typing import Sequence, TYPE_CHECKING, Union, Optional
 
 from sqlalchemy import select, Result
 from sqlalchemy.exc import IntegrityError
@@ -56,7 +56,33 @@ class ProductsRepository:
             raise CustomException(
                 msg=f"{CLASS} with {text_error} not found"
             )
-        print("OOOOOOOOORRRRRM MODEL DICT", orm_model.to_dict())
+        return orm_model
+
+    async def get_one_complex_full(
+            self,
+            id: int = None,
+            slug: str = None,
+    ):
+        stmt_select = select(Product)
+        if id:
+            stmt_filter = stmt_select.where(Product.id == id)
+        else:
+            stmt_filter = stmt_select.where(Product.slug == slug)
+
+        stmt = stmt_filter.options(
+            joinedload(Product.images),
+            joinedload(Product.brand).joinedload(Brand.image),
+            joinedload(Product.rubrics).joinedload(Rubric.image),
+        ).order_by(Product.id)
+
+        result: Result = await self.session.execute(stmt)
+        orm_model: Brand | None = result.unique().scalar_one_or_none()
+
+        if not orm_model:
+            text_error = f"id={id}" if id else f"slug={slug!r}"
+            raise CustomException(
+                msg=f"{CLASS} with {text_error} not found"
+            )
         return orm_model
 
     async def get_one(
@@ -136,7 +162,7 @@ class ProductsRepository:
                 orm_model.rubrics.append(rubric_orm_model)
             await self.session.commit()
             await self.session.refresh(orm_model)
-            self.logger.info("%r %r was successfully created" % (CLASS, orm_model))
+            self.logger.info("%s %r was successfully created" % (CLASS, orm_model))
             self.logger.info("Rubrics were successfully added to product")
         except IntegrityError as error:
             self.logger.error(f"Error while orm_model with relations creating", exc_info=error)
@@ -195,4 +221,43 @@ class ProductsRepository:
             self.logger.error("Error while deleting data from database", exc_info=exc)
             raise CustomException(
                 msg="Error while deleting %r from database" % orm_model
+            )
+
+    async def edit_one_with_relations(
+            self,
+            instance:  Union["ProductUpdate", "ProductPartialUpdate"],
+            orm_model: Product,
+            rubric_orms: Optional[list[Rubric]] = None,
+            image_schemas: Optional[list[str]] = None,
+            is_partial: bool = False
+    ):
+        if is_partial:
+            instance.start_price = instance.start_price or orm_model.start_price
+            instance.discount = instance.discount if isinstance(instance.discount, int) else orm_model.discount
+        for key, val in instance.model_dump(
+                exclude_unset=is_partial,
+                exclude_none=is_partial,
+        ).items():
+            setattr(orm_model, key, val)
+
+        self.logger.warning(f"Editing %r in database" % orm_model)
+        try:
+
+            if rubric_orms:
+                orm_model.rubrics.clear()
+                for rubric_orm_model in rubric_orms:
+                    orm_model.rubrics.append(rubric_orm_model)
+
+            if image_schemas:
+                for old_image in orm_model.images:
+                    await self.session.delete(old_image)
+
+            await self.session.commit()
+            await self.session.refresh(orm_model)
+            self.logger.info("%r %r was successfully edited" % (CLASS, orm_model))
+            self.logger.info("Rubrics list was successfully edited")
+        except IntegrityError as exc:
+            self.logger.error("Error occurred while editing data in database", exc_info=exc)
+            raise CustomException(
+                msg=Errors.already_exists_titled(instance.title)
             )
