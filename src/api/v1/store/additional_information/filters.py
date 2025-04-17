629c7c93b2c765a8c6cdb6ebb4cdf7da2201ba8d
Primero1800 <Primero@inbox.ru>
2025-04-17 15:21:19 +0300

    feat(app) AdditionalInformation filter complex added

--- src/api/v1/store/additional_information/filters.py
+++ src/api/v1/store/additional_information/filters.py
@@ -9,13 +9,22 @@ from src.core.models import AdditionalInformation
 
 class AddInfoFilter(Filter):
     order_by: Optional[list[str]] = ['id']
-    product__id:  Optional[int] = Field(default=None, description="Filter by product id")
-    product__title__like: Optional[str] = Field(default=None, description="Filter by product title contains", )
-    available: Optional[bool] = Field(default=None, description="Filter if available")
-    search_for: Optional[str] = None
+    guarantee__like: Optional[str] = Field(default=None, description="Filter by product guarantee contains", )
 
     class Constants(Filter.Constants):
         model = AdditionalInformation
+
+    class Config:
+            populated_by_name = True
+
+
+class AddInfoFilterComplex(Filter):
+    product__available: Optional[bool] = Field(default=None, description="Filter if available")
+    product__id:  Optional[int] = Field(default=None, description="Filter by product id")
+    product__title__like: Optional[str] = Field(default=None, description="Filter by product title contains", )
+    search_for: Optional[str] = None
+
+    class Constants(AddInfoFilter.Constants):
         search_field_name = "search_for"
         search_model_fields = ["product__title", "product__description"]
 
