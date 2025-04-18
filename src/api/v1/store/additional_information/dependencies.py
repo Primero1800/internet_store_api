3a6be94a1979e2d960d1986ba98036b21b75ebdd
Primero1800 <Primero@inbox.ru>
2025-04-18 08:52:42 +0300

    feat(app) AdditionalInformation dependency get_one added

---
+++ src/api/v1/store/additional_information/dependencies.py
@@ -0,0 +1,19 @@
+from typing import TYPE_CHECKING
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.config import DBConfigurer
+from .service import AddInfoService
+
+if TYPE_CHECKING:
+    from src.core.models import AdditionalInformation
+
+
+async def get_one_simple(
+    id: int,
+    session: AsyncSession = Depends(DBConfigurer.session_getter)
+) -> "AdditionalInformation":
+    service: AddInfoService = AddInfoService(
+        session=session
+    )
+    return await service.get_one(id=id, to_schema=False)
