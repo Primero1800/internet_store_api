ba83f162a53cc79b2c3c9a3a0bc22e4f4b0edb29
Primero1800 <Primero@inbox.ru>
2025-04-18 08:43:31 +0300

    feat(app) AdditionalInformation filter outer filter added

--- src/api/v1/store/additional_information/filters.py
+++ src/api/v1/store/additional_information/filters.py
@@ -1,10 +1,12 @@
 from decimal import Decimal
 from typing import Optional
 
+from fastapi_filter import FilterDepends, with_prefix
 from fastapi_filter.contrib.sqlalchemy import Filter
 from pydantic import Field
 
 from src.core.models import AdditionalInformation
+from ..products.filters import ProductFilterShort
 
 
 class AddInfoFilter(Filter):
@@ -14,19 +16,13 @@ class AddInfoFilter(Filter):
     class Constants(Filter.Constants):
         model = AdditionalInformation
 
-    class Config:
-            populated_by_name = True
 
-
-class AddInfoFilterComplex(Filter):
-    product__available: Optional[bool] = Field(default=None, description="Filter if available")
-    product__id:  Optional[int] = Field(default=None, description="Filter by product id")
-    product__title__like: Optional[str] = Field(default=None, description="Filter by product title contains", )
+class AddInfoFilterComplex(AddInfoFilter):
+    product_id: Optional[int] = Field(default=None, description="Filter by product id")
+    product: Optional[ProductFilterShort] = FilterDepends(with_prefix("product", ProductFilterShort))
     search_for: Optional[str] = None
 
     class Constants(AddInfoFilter.Constants):
+        model = AdditionalInformation
         search_field_name = "search_for"
         search_model_fields = ["product__title", "product__description"]
-
-    class Config:
-            populated_by_name = True
