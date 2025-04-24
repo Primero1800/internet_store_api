004b0f0dd58d2f99d28282cc8601827c94d6a1e4
Primero1800 <Primero@inbox.ru>
2025-04-24 09:53:20 +0300

    feat(app) User exceptions added

--- src/api/v1/users/exceptions.py
+++ src/api/v1/users/exceptions.py
@@ -1,11 +1,18 @@
 from src.tools.exceptions import CustomException
 
+CLASS = "User"
 
 class Errors:
 
-    HANDLER_MESSAGE = "Handled by Users exception handler"
+    HANDLER_MESSAGE = f"Handled by {CLASS}s exception handler"
 
     DATABASE_ERROR = "Error occurred while changing database data"
+
+    NOT_FOUND = f"{CLASS} not found"
+
+    @staticmethod
+    def user_not_found_id(id: int):
+        return f"{CLASS} with id={id} not found"
 
 
 class NoSessionException(CustomException):
