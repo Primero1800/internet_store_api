1908207af5ea371173986993fc68726113c3ee47
Primero1800 <Primero@inbox.ru>
2025-04-24 12:53:18 +0300

    feat(app) Users moved to new package user

--- src/api/v1/store/votes/utils.py
+++ src/api/v1/store/votes/utils.py
@@ -21,8 +21,8 @@ async def get_schema_from_orm(
     if maximized or 'product' in relations:
         from ..products.utils import get_short_schema_from_orm as get_short_product_schema_from_orm
         product_short = await get_short_product_schema_from_orm(orm_model.product)
-    if 'product' in relations:
-        return product_short
+        if 'product' in relations:
+            return product_short
 
     if maximized or 'user' in relations:
         if orm_model.user:
@@ -30,8 +30,8 @@ async def get_schema_from_orm(
             user_short = await get_short_user_schema_from_orm(orm_model.user)
         else:
             user_short = None
-    if 'user' in relations:
-        return user_short
+        if 'user' in relations:
+            return user_short
 
     return VoteRead(
         **short_schema.model_dump(),
