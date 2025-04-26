de163520a7c5160909487adb62f5c67720c33795
Primero1800 <Primero@inbox.ru>
2025-04-26 13:58:00 +0300

    feat(app) Carts exceptions added

---
+++ src/api/v1/carts/exceptions.py
@@ -0,0 +1,14 @@
+CLASS = "Cart"
+_CLASS = "cart"
+
+class Errors:
+
+    HANDLER_MESSAGE = f"Handled by {CLASS}s exception handler"
+
+    DATABASE_ERROR = "Error occurred while changing database data"
+
+    NOT_EXISTS = f"{CLASS} doesn't exist"
+
+    ALREADY_EXISTS = f"{CLASS} already exists"
+
+    NO_RIGHTS = "You are not authorized for this operation"
