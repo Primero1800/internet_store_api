9e019622e7ac51595f6902046d6154943cb59a86
Primero1800 <Primero@inbox.ru>
2025-05-01 15:12:19 +0300

    feat(app) SessionCart dependencies added

--- src/api/v1/carts/dependencies.py
+++ src/api/v1/carts/dependencies.py
@@ -1,13 +1,18 @@
-from typing import TYPE_CHECKING
-from fastapi import Depends, Form
+from typing import TYPE_CHECKING, Union
+from fastapi import Depends, Form, status
+from fastapi.responses import ORJSONResponse
+from fastapi_sessions.backends.session_backend import BackendError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.config import DBConfigurer
+from src.core.sessions.fastapi_sessions_config import verifier_or_none, SessionData
 from .service import CartsService
-from ..users.user.dependencies import current_user
+from .exceptions import Errors
+from ..users.user.dependencies import current_user, current_user_or_none
 
 if TYPE_CHECKING:
     from src.core.models import Cart, User, CartItem
+    from src.api.v1.carts.session_cart import SessionCart
 
 CLASS = "Cart"
 
@@ -44,3 +49,31 @@ async def get_or_create_cart_item(
         cart=cart,
         product_id=product_id
     )
+
+
+async def user_cookie_or_error(
+        user: "User" = Depends(current_user_or_none),
+        session_data: SessionData = Depends(verifier_or_none),
+) -> Union["User", SessionData, ORJSONResponse]:
+    if user:
+        return user
+    if session_data and not isinstance(session_data, BackendError):
+        return session_data
+    return ORJSONResponse(
+        status_code=status.HTTP_403_FORBIDDEN,
+        content={
+            "message": Errors.HANDLER_MESSAGE,
+            "detail": "No authentication or session provided",
+        }
+    )
+
+
+async def get_or_create_cart_sessioned(
+        cart_type: Union["User", SessionData, ORJSONResponse] = Depends(user_cookie_or_error),
+        session: AsyncSession = Depends(DBConfigurer.session_getter)
+) -> Union["Cart", "SessionCart"]:
+    service: CartsService = CartsService(
+        session=session
+    )
+    return await service.get_or_create(cart_type=cart_type)
+
