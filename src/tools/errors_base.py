ee7c578b6c742aba41786656d9e2ae0a072a6bf3
Primero1800 <Primero@inbox.ru>
2025-05-05 14:51:00 +0300

    feat(app) ErrorsCarts added

--- src/tools/errors_base.py
+++ src/tools/errors_base.py
@@ -21,3 +21,23 @@ class ErrorsBase:
     @classmethod
     def NO_RIGHTS(cls):
         return "You are not authorized for this operation"
+
+    @staticmethod
+    def USER_NOT_VERIFIED():
+        return "User is not verified"
+
+    @staticmethod
+    def BAD_CREDENTIALS_OR_NOT_ACTIVE():
+        return "Bad credentials or user is not active"
+
+    @staticmethod
+    def INVALID_PASSWORD():
+        return "Invalid password"
+
+    @staticmethod
+    def INACTIVE_USER():
+        return "User is inactive"
+
+    @staticmethod
+    def INVALID_TOKEN():
+        return "Invalid token"
