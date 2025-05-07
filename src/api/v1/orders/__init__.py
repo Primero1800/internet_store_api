ba05fc4f62c00f82baecf8b6682cbb485caf021d
Primero1800 <Primero@inbox.ru>
2025-05-07 09:23:31 +0300

    feat(app) orders routers added

--- src/api/v1/orders/__init__.py
+++ src/api/v1/orders/__init__.py
@@ -13,19 +13,19 @@ router = APIRouter()
 router.include_router(
     order_router,
     tags=[settings.tags.ORDERS_TAG,],
-    prefix=settings.tags.ORDERS_TAG
+    prefix=settings.tags.ORDERS_PREFIX
 )
 
 
 router.include_router(
     person_router,
     tags=[settings.tags.PERSONS_TAG,],
-    prefix=settings.tags.PERSONS_TAG
+    prefix=settings.tags.PERSONS_PREFIX
 )
 
 
 router.include_router(
     address_router,
     tags=[settings.tags.ADDRESSES_TAG,],
-    prefix=settings.tags.ADDRESSES_TAG
+    prefix=settings.tags.ADDRESSES_PREFIX
 )
