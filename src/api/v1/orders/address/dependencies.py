fdf150aa54e48ba2df4473a6b5b775a1acce66cc
Primero1800 <Primero@inbox.ru>
2025-05-07 16:04:35 +0300

    feat(app) Address dependencies added

---
+++ src/api/v1/orders/address/dependencies.py
@@ -0,0 +1,57 @@
+from typing import Union, TYPE_CHECKING
+
+from fastapi import Depends
+from fastapi.responses import ORJSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.api.v1.users.user.dependencies import user_cookie_or_error
+from src.core.config import DBConfigurer
+
+.service import AddressesService
+
+if TYPE_CHECKING:
+    from src.core.models import (
+        Address,
+        User,
+    )
+    from src.core.sessions.fastapi_sessions_config import SessionData
+    from .session_address import SessionAddress
+
+
+async def get_one_session(
+        obj_type: Union["User", "SessionData", ORJSONResponse] = Depends(user_cookie_or_error),
+        session: AsyncSession = Depends(DBConfigurer.session_getter)
+) -> Union["Address", "SessionAddress"]:
+    service: AddressesService = AddressesService(
+        session=session
+    )
+    return await service.get_one(
+        obj_type=obj_type,
+        to_schema=False,
+    )
+
+
+async def get_one_session_full(
+        obj_type: Union["User", "SessionData", ORJSONResponse] = Depends(user_cookie_or_error),
+        session: AsyncSession = Depends(DBConfigurer.session_getter)
+) -> Union["Address", "SessionAddress"]:
+    service: AddressesService = AddressesService(
+        session=session
+    )
+    return await service.get_one_complex(
+        obj_type=obj_type,
+        to_schema=False,
+    )
+
+
+async def get_one_simple(
+        user_id: int,
+        session: AsyncSession = Depends(DBConfigurer.session_getter)
+) -> "Address":
+    service: AddressesService = AddressesService(
+        session=session
+    )
+    return await service.get_one(
+        user_id=user_id,
+        to_schema=False,
+    )
