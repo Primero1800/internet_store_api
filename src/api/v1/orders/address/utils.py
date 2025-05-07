b51e6b10053e2ebbc7d75d6555085bfd7b796abb
Primero1800 <Primero@inbox.ru>
2025-05-07 16:17:27 +0300

    feat(app) Address utils added

---
+++ src/api/v1/orders/address/utils.py
@@ -0,0 +1,57 @@
+from typing import TYPE_CHECKING, Union, Any
+
+from fastapi.responses import ORJSONResponse
+
+from .schemas import (
+    AddressRead,
+    AddressShort,
+)
+
+if TYPE_CHECKING:
+    from src.core.models import (
+        Address,
+    )
+    from .session_address import SessionAddress
+
+
+async def get_schema_from_orm(
+        orm_model: Union["Address", "SessionAddress", ORJSONResponse],
+        maximized: bool = True,
+        relations: list | None = [],
+) -> Any:
+    # BRUTE FORCE VARIANT
+
+    if isinstance(orm_model, ORJSONResponse):
+        return orm_model
+
+    # short_schema: CartShort = await get_short_schema_from_orm(orm_model=orm_model)
+
+    dict_to_push_to_schema = orm_model.to_dict()
+    if 'user' in dict_to_push_to_schema:
+        del dict_to_push_to_schema['user']
+
+    user_short = None
+    if maximized or 'user' in relations:
+        if orm_model.user:
+            from src.api.v1.users.user.utils import get_short_schema_from_orm as get_short_user_schema_from_orm
+            user_short = await get_short_user_schema_from_orm(orm_model.user)
+        if 'user' in relations:
+            return user_short
+
+    return AddressRead(
+        **dict_to_push_to_schema,
+        user=user_short
+    )
+
+
+async def get_short_schema_from_orm(
+        orm_model: Union["Address", "SessionAddress", ORJSONResponse]
+) -> AddressShort | ORJSONResponse:
+
+    if isinstance(orm_model, ORJSONResponse):
+        return orm_model
+
+    # BRUTE FORCE VARIANT
+    return AddressShort(
+        **orm_model.to_dict(),
+    )
