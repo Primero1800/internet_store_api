a6c5b49ddfe1a06ebb029375875371d207cf743a
Primero1800 <Primero@inbox.ru>
2025-05-07 16:13:36 +0300

    feat(app) Address repository added

---
+++ src/api/v1/orders/address/repository.py
@@ -0,0 +1,157 @@
+import logging
+from fastapi import status
+from typing import Sequence, TYPE_CHECKING, Union
+
+from sqlalchemy import select, Result
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
+
+from src.core.models import Address
+from src.tools.exceptions import CustomException
+from .exceptions import Errors
+
+if TYPE_CHECKING:
+    from .schemas import (
+        AddressCreate,
+        AddressUpdate,
+        AddressPartialUpdate,
+    )
+    from .filters import AddressFilter
+
+
+CLASS = "Address"
+
+
+class AddressesRepository:
+    def __init__(
+            self,
+            session: AsyncSession,
+    ):
+        self.session = session
+        self.logger = logging.getLogger(__name__)
+
+    async def get_one_complex(
+            self,
+            user_id: int = None,
+            maximized: bool = True,
+            relations: list = [],
+    ):
+        stmt_filter = select(Address).where(Address.user_id == user_id)
+
+        options_list = []
+
+        if maximized or "user" in relations:
+            options_list.append(joinedload(Address.user))
+
+        stmt = stmt_filter.options(*options_list)
+
+        result: Result = await self.session.execute(stmt)
+        orm_model: Address | None = result.unique().scalar_one_or_none()
+
+        if not orm_model:
+            text_error = f"user_id={user_id}"
+            raise CustomException(
+                msg=f"{CLASS} with {text_error} not found"
+            )
+        return orm_model
+
+    async def get_one(
+            self,
+            user_id: int
+    ):
+        orm_model = await self.session.get(Address, user_id)
+        if not orm_model:
+            text_error = f"user_id={user_id}"
+            raise CustomException(
+                msg=f"{CLASS} with {text_error} not found"
+            )
+        return orm_model
+
+    async def get_all(
+            self,
+            filter_model: "AddressFilter",
+    ) -> Sequence:
+
+        query_filter = filter_model.filter(select(Address))
+        stmt_filtered = filter_model.sort(query_filter)
+
+        stmt = stmt_filtered.order_by(Address.user_id)
+
+        result: Result = await self.session.execute(stmt)
+        return result.unique().scalars().all()
+
+    async def get_all_full(
+            self,
+            filter_model: "AddressFilter",
+    ) -> Sequence:
+
+        query_filter = filter_model.filter(select(Address))
+        stmt_filtered = filter_model.sort(query_filter)
+
+        stmt = stmt_filtered.options(
+            joinedload(Address.user),
+        ).order_by(Address.user_id)
+
+        result: Result = await self.session.execute(stmt)
+        return result.unique().scalars().all()
+
+    async def get_orm_model_from_schema(
+            self,
+            instance: Union["AddressCreate", "AddressUpdate", "AddressPartialUpdate"]
+    ):
+        orm_model: Address = Address(**instance.model_dump())
+        return orm_model
+
+    async def create_one_empty(
+            self,
+            orm_model: Address
+    ):
+        try:
+            self.session.add(orm_model)
+            await self.session.commit()
+            await self.session.refresh(orm_model)
+            self.logger.info("%r %r was successfully created" % (CLASS, orm_model))
+        except IntegrityError as error:
+            self.logger.error(f"Error while orm_model creating", exc_info=error)
+            raise CustomException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                msg=Errors.already_exists_id(orm_model.user_id)
+            )
+
+    async def delete_one(
+            self,
+            orm_model: Address,
+    ) -> None:
+        try:
+            self.logger.info(f"Deleting %r from database" % orm_model)
+            await self.session.delete(orm_model)
+            await self.session.commit()
+        except IntegrityError as exc:
+            self.logger.error("Error while deleting data from database", exc_info=exc)
+            raise CustomException(
+                msg="Error while deleting %r from database" % orm_model
+            )
+
+    async def edit_one_empty(
+            self,
+            instance:  Union["AddressUpdate", "AddressPartialUpdate"],
+            orm_model: Address,
+            is_partial: bool = False
+    ):
+        for key, val in instance.model_dump(
+                exclude_unset=is_partial,
+                exclude_none=is_partial,
+        ).items():
+            setattr(orm_model, key, val)
+
+        self.logger.warning(f"Editing %r in database" % orm_model)
+        try:
+            await self.session.commit()
+            await self.session.refresh(orm_model)
+        except IntegrityError as exc:
+            self.logger.error("Error occurred while editing data in database", exc_info=exc)
+            raise CustomException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                msg=Errors.DATABASE_ERROR()
+            )
