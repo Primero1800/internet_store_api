d01d144d2904a5579224b8f563a40c56f0771e1d
Primero1800 <Primero@inbox.ru>
2025-05-09 14:59:22 +0300

    feat(app) Orders get_one_complex dependency added

--- src/api/v1/orders/order/dependencies.py
+++ src/api/v1/orders/order/dependencies.py
@@ -4,18 +4,38 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.config import DBConfigurer
 from .service import OrdersService
+from ...users.user.dependencies import current_user
 
 if TYPE_CHECKING:
-    from src.core.models import Order
+    from src.core.models import Order, User
 
 CLASS = "Order"
 
 
 async def get_one_simple(
     id: int,
+    user: "User" = Depends(current_user),
     session: AsyncSession = Depends(DBConfigurer.session_getter)
 ) -> "Order":
     service: OrdersService = OrdersService(
         session=session
     )
-    return await service.get_one(id=id, to_schema=False)
+    return await service.get_one(
+        id=id,
+        user=user,
+        to_schema=False
+    )
+
+async def get_one_complex(
+    id: int,
+    user: "User" = Depends(current_user),
+    session: AsyncSession = Depends(DBConfigurer.session_getter)
+) -> "Order":
+    service: OrdersService = OrdersService(
+        session=session
+    )
+    return await service.get_one_complex(
+        id=id,
+        user=user,
+        to_schema=False
+    )
