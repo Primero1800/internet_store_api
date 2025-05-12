fff5947256bb26670feb35ad4723ee3f3b5789ed
Primero1800 <Primero@inbox.ru>
2025-05-12 10:50:25 +0300

    feat(app) Utils get_contents two functions added

--- src/api/v1/orders/order/utils.py
+++ src/api/v1/orders/order/utils.py
@@ -1,5 +1,5 @@
-from decimal import Decimal
-from typing import Union, TYPE_CHECKING
+from decimal import Decimal, ROUND_HALF_UP
+from typing import Union, TYPE_CHECKING, Any, Optional
 
 from fastapi.encoders import jsonable_encoder
 
@@ -57,20 +57,21 @@ async def get_short_schema_from_orm(
 
 
 async def get_normalized_order_content(
-        cart: Union["Cart", "SessionCart"]
+        cart_items: list[Any],
+        cart: Optional[Union["Cart", "SessionCart"]] = None,
 ) -> list[dict]:
 
     from src.api.v1.carts import utils as carts_utils
-    cart = await carts_utils.get_schema_from_orm(cart)
-    return [jsonable_encoder(item) for item in cart.cart_items]
+    if cart:
+        cart = await carts_utils.get_schema_from_orm(cart)
+        return [jsonable_encoder(item) for item in cart.cart_items]
+    cart_items = [await carts_utils.get_item_schema_from_orm(item) for item in cart_items]
+    return [jsonable_encoder(item) for item in cart_items]
 
 
 async def get_normalized_total_cost(
         cart: Union["Cart", "SessionCart"]
 ):
     return Decimal(
-                sum(
-                    item.quantity*item.price if hasattr(item, "quantity")
-                    else item['quantity']*item['price'] for item in cart.cart_items
-                )
-            ).quantize(Decimal("0.01"))
+                sum(item.quantity * item.price for item in cart.cart_items)
+            ).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
